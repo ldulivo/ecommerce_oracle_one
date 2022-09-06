6fa0b85f7c9151233daca1db01149ebd95aca50a
ldulivo <ldulivo@gmail.com>
2022-09-05 23:23:57 -0300

    Added dialog modal

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import imagen from '../img/unsplash_6FDXGY9J6y4.png'
+import { SvgZoomIn } from '../svg/SvgZoomIn'
 
 export const Card = () => {
   return (
-    <a href="#" className="card">
-      <div className="product-img">
-        <img src={imagen} alt="imagen" />
-      </div>
-      <h3 className="product-name">Producto sdf</h3>
-      <p className="product-price">$ 60</p>
-      <span>Ver producto</span>
-    </a>
+    <div className="card">
+      <SvgZoomIn img={imagen}/>
+      <a href="#" className="card-anchor">
+        <div className="product-img">
+          <img src={imagen} alt="imagen"/>
+        </div>
+        <h3 className="product-name">Producto sdf</h3>
+        <p className="product-price">$ 60</p>
+        <span>Ver producto</span>
+      </a>
+    </div>
   )
 }
